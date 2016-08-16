fecd4d72422544f626915e620daed3f6ee64feb3
dawn <602468816@qq.com>
2016-08-16 16:03:32 +0800

    xx

--- app/src/main/java/com/dw/course/MainActivity.java
+++ app/src/main/java/com/dw/course/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //999
         setContentView(R.layout.activity_main);
     }
 }
